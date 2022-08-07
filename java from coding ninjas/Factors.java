c2d5f8f5b140268a937934e4bf4f064b6754c406
kanishk singhal <72912708+kanix-1801@users.noreply.github.com>
2022-08-07 10:12:23 +0530

    Add files via upload

--- java from coding ninjas/Factors.java
+++ java from coding ninjas/Factors.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 class Factors{
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         for(int j = 2; j<n; j++){
             if(n%j==0){
-                System.out.print(j+"");
+                System.out.print(j+",");
             }
             else{}
         }   
