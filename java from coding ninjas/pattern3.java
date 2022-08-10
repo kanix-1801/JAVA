b30e191a349a29ef64ad1af02561b75797c74e8d
kanishk singhal <72912708+kanix-1801@users.noreply.github.com>
2022-08-10 17:36:50 +0530

    Add files via upload

---
+++ java from coding ninjas/pattern3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class pattern3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        while(i <= n){
+            int j = 1;
+            while(j<=n){
+                System.out.print(j);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
+// input-->
+// 4
+// output-->
+// 1234
+// 1234
+// 1234
+// 1234
